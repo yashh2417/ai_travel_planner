24678c4ef4c921868349f1772a5d4c21fa872818
Yash <yashh2417@gmail.com>
2025-07-07 13:45:13 +0530

    setup.py build

--- setup.py
+++ setup.py
@@ -1,4 +1,31 @@
 from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements()->List[str]:
+    """
+    This function will return list of requirements
+    """
+    requirement_list:List[str] = []
+    
+    try:
+        # Open and read the requirements.txt file
+        with open('requirements.txt', 'r') as file:
+            # Read lines from the file
+            lines = file.readlines()
+            # Process each line
+            for line in lines:
+                # Strip whitespace and newline characters
+                requirement = line.strip()
+                # Ignore empty lines and -e .
+                if requirement and requirement != '-e .':
+                    requirement_list.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found.")
+
+    
+        
+    return requirement_list
+print(get_requirements())
 
 setup(
     name="ai_travel_planner",
@@ -7,7 +34,7 @@ setup(
     author="Yash",
     author_email="yashh2417@gmail.com",
     packages=find_packages(),
-    install_requires=[],
-    python_requires="3.10",
+    install_requires=get_requirements(),
+    python_requires=">=3.10"
 )
 
