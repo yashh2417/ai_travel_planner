76a4fc750bd44436e71710f5ce5b0b9996dd67ec
Yash <yashh2417@gmail.com>
2025-07-07 13:01:12 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ai_travel_assistant",
+    version="0.1.0",
+    description="Agentic AI Travel Agent helps you plan smart, budget-friendly trips by using real-time data on weather, events, hotels, transport, and more — all powered by AI.",
+    author="Yash",
+    author_email="yashh2417@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires="3.10",
+)
+
